c8fd5954094e47fae925b8f2c2906992455c5a07
NurGeo <anzpro@gmail.com>
2022-10-17 15:23:42 +0500

    Внесены правки в assert.js и assert2html.js:   1. Исправлены ошибки при выводе в html;   2.Теперь тесты не обязаны возвращать строки;   3. Добавлены угловые скобки <undefined> not to be <any value>.

--- dependencies/asserts/assert.js
+++ dependencies/asserts/assert.js
@@ -7,22 +7,22 @@
  * Это основания задача которые должны решить функции утверждения.
  */
 
-/** Функция для объединения нескольких проверок в один набор */
-export function assertSuite(suiteDesc, cb) {
-  cb();
-  return `${suiteDesc} - success runned`;
-}
+// /** Функция для объединения нескольких проверок в один набор */
+// export function assertSuite(suiteDesc, cb) {
+//   cb();
+//   return `${suiteDesc} - success runned`;
+// }
 /** это утверждение проверяет строгое равенство a и b */
 export function assertToBe(assertDescription, a, b) {
   if (a === b) return `${assertDescription} - success runned`;
-  const errMsg = `Test Assertion Error: ${arg2string(a)} not to be ${arg2string(b)}`
+  const errMsg = `Test Assertion Error: <${arg2string(a)}> not to be <${arg2string(b)}>`
   throwError(assertDescription, errMsg);
 }
 
 /** это утверждение проверяет строгое неравенство a и b */
 export function assertNotToBe(assertDescription, a, b) {
   if (a !== b) return `${assertDescription} - success runned`;
-  const errMsg = `Test Assertion Error: ${arg2string(a)} to be ${arg2string(b)}`
+  const errMsg = `Test Assertion Error: <${arg2string(a)}> to be <${arg2string(b)}>`
   throwError(assertDescription, errMsg);
 }
 
@@ -32,7 +32,7 @@ export function assertEqual(assertDescription, a, b) {
   const jsonB = JSON.stringify(b);
   if (jsonA === jsonB)
     return `${assertDescription} - success runned`;
-  const errMsg = `Test Assertion Error: ${jsonA} not equal ${jsonB}`
+  const errMsg = `Test Assertion Error: <${jsonA}> not equal <${jsonB}>`
   throwError(assertDescription, errMsg);
 }
 
@@ -45,7 +45,7 @@ export function assertNotEqual(assertDescription, a, b) {
   }
   const jsonA = JSON.stringify(a);
   const jsonB = JSON.stringify(b);
-  const errMsg = `Test Assertion Error: ${jsonA} equal ${jsonB}`
+  const errMsg = `Test Assertion Error: <${jsonA}> equal <${jsonB}>`
   throwError(assertDescription, errMsg);
 }
 
@@ -57,7 +57,7 @@ export function assertThrow(assertDescription, cb, errString) {
     if (!errString || (eAsString.includes(errString))) {
       return `${assertDescription} - success runned`;
     }
-    const errMsg = `Test Assertion Error: ${cb.name} throwed an exception, but ${errString} not inculdes by "${eAsString}"`
+    const errMsg = `Test Assertion Error: ${cb.name} throwed an exception, but <${errString}> not inculdes by <${eAsString}>`
     throwError(assertDescription, errMsg);
   }
   const errMsg = `Test Assertion Error: ${cb.name} did not throw an exception`
@@ -73,6 +73,6 @@ function arg2string(arg) {
 
 /** вспомогательная фукнция, выкидывает исключение */
 function throwError(testDsc, errMsg) {
-  const fullErrorMsg = `testing: ${testDsc}\n${errMsg}`;
+  const fullErrorMsg = `${testDsc}\n${errMsg}`;
   throw Error(fullErrorMsg);
 }
--- dependencies/asserts/assert2html.js
+++ dependencies/asserts/assert2html.js
@@ -1,70 +1,73 @@
 /** модуль позволяет делать тест assert.js через браузер.
  * */
 
+/** Вызвает тесты и добавляет их результаты в html документ. */
 export function testsForHtml(callBacks) {
   for (let testCb of callBacks) {
     assert2html(testCb);
   }
 }
 
+/** Вызывает один тест (testCb) и добавить его результат в html документ. */
 export function assert2html(testCb) {
   const testEl = document.createElement('div');
   setElAttribute(testEl, 'test');
   document.body.appendChild(testEl)
 
+  const funcName = testCb.name;
   try {
-    updateElement(testEl, string2html(testCb()));
+    testCb();
+    updateElement(testEl, string2html(`Success: ${funcName} - success runned`));
   } catch (e) {
     const err = `${e.stack}`;
 
-    updateElement(testEl, string2html(err));
+    const errNameIndex = err.indexOf(': ') + 2;
+    let errName = err.substring(0, errNameIndex);
+    if (errName === 'Error: ') errName = 'TestError: ';
+    const firstLineIndex = err.indexOf('\n');
+    const errDesc = err.substring(errNameIndex, firstLineIndex);
+    const otherLines = err.substring(firstLineIndex);
+    updateElement(testEl, string2html(`${errName}${funcName}(${errDesc})${otherLines}`));
   }
 }
 
-function setElAttribute(el, value, attrName = 'class') {
-  if (attrName === 'class') {
-    const clsName = getClsName(el.tagName.toLowerCase(), value);
-    if (clsName) el.setAttribute(attrName, clsName);
-  } else {
-    el.setAttribute(attrName, value);
-  }
-}
-
-function updateElement(elem, elContent) {
-  elem.innerHTML = elContent;
-}
-
+/** Показывает результат выполнения теста в html странице. */
 function string2html(str) {
   const errEl = document.createElement('div');
-  if (str.startsWith('Error')) {
-    const strArr = str.split('\n');
-    const firstErrLine = strArr.shift();
-    const prefix = str.startsWith('Error: ') ? 'Error: ' : 'Error'
-    errEl.appendChild(createTestElement(firstErrLine, 'h1', prefix, 'test-desc'));
-    if(strArr[0].startsWith('Test Assertion Error: ')) {
-      const secondErrLine = strArr.shift();
-      errEl.appendChild(createTestElement(secondErrLine, 'h2', 'Test Assertion Error: ', 'test-err'));
-    }
-    else {
-      const secondErrLine = 'Runtime Error: the error is not a test';
-      errEl.appendChild(createTestElement(secondErrLine, 'h2', 'Runtime Error: ', 'test-err'));
-    }
-
-    const stackBlockEl = document.createElement('div');
-    setElAttribute(stackBlockEl, 'test-stack');
-    errEl.appendChild(stackBlockEl);
-
-    strArr.forEach((strItem) => {
-      const txtLine = strItem.trim();
-      stackBlockEl.appendChild(createTestElement(txtLine, 'p', 'at ', 'test-stack'));
-    });
+  const leadText = str.substring(0, str.indexOf(': ') + 2);
+  if (leadText !== 'Success: ') {
+    appendError(errEl, str, leadText);
   } else {
-    const firstSuccessLine = 'Success: ' + str;
-    errEl.appendChild(createTestElement(firstSuccessLine, 'h1', 'Success: ', 'test-success'));
+    errEl.appendChild(createTestElement(str, 'h1', 'Success: ', 'test-success'));
   }
   return errEl.outerHTML;
 }
 
+/** Показывает результат выполнения теста в html странице. */
+function appendError(errEl, str, leadText) {
+  const strArr = str.split('\n');
+  const firstErrLine = strArr.shift();
+  errEl.appendChild(createTestElement(firstErrLine, 'h1', leadText, 'test-desc'));
+  if(strArr[0].startsWith('Test Assertion Error: ')) {
+    const secondErrLine = strArr.shift();
+    errEl.appendChild(createTestElement(secondErrLine, 'h2', 'Test Assertion Error: ', 'test-err'));
+  }
+  else {
+    const secondErrLine = 'Runtime Error: the error is not a test';
+    errEl.appendChild(createTestElement(secondErrLine, 'h2', 'Runtime Error: ', 'test-err'));
+  }
+
+  const stackBlockEl = document.createElement('div');
+  setElAttribute(stackBlockEl, 'test-stack');
+  errEl.appendChild(stackBlockEl);
+
+  strArr.forEach((strItem) => {
+    const txtLine = strItem.trim();
+    stackBlockEl.appendChild(createTestElement(txtLine, 'p', 'at ', 'test-stack'));
+  });
+}
+
+/** Создает на страничке html новый элемент теста. */
 function createTestElement(textCnt, elType, prefixText, clsName) {
   const i = prefixText.length;
   const span = document.createElement('span');
@@ -78,6 +81,17 @@ function createTestElement(textCnt, elType, prefixText, clsName) {
   return p;
 }
 
+/** Установить атрибут html элемента. */
+function setElAttribute(el, value, attrName = 'class') {
+  if (attrName === 'class') {
+    const clsName = getClsName(el.tagName.toLowerCase(), value);
+    if (clsName) el.setAttribute(attrName, clsName);
+  } else {
+    el.setAttribute(attrName, value);
+  }
+}
+
+/** Выдает имена w3.css из переданного элемента html и доменного описания. */
 function getClsName(elType, clsName) {
   const token = `${elType}:${clsName}`;
   if (token === 'div:test') return 'w3-card'
@@ -91,4 +105,9 @@ function getClsName(elType, clsName) {
   else if (token === 'span:test-stack') return 'w3-text-red';
   else if (token === 'span:test-success') return 'w3-text-green';
   else throw Error('not valid element type and class name');
+}
+
+/** Обновить содержимое html элемента. */
+function updateElement(elem, elContent) {
+  elem.innerHTML = elContent;
 }
