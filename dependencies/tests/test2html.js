63c737f921ac28c1be011e6cd0813924891bbcc8
NurGeo <anzpro@gmail.com>
2022-10-20 11:00:29 +0500

    Проведен довольно большой рефакторинг файла test2html.js

--- dependencies/tests/test2html.js
+++ dependencies/tests/test2html.js
@@ -4,8 +4,8 @@ import { testRunner } from '../../../dependencies/tests/test.js';
 
 /** Выполняет тесты и добавляет их результаты в html документ. */
 export function runTestsForHtml() {
-  document.getElementById('showErrors').onclick = updateDetails;
-  document.getElementById('showSuccesses').onclick = updateDetails;
+  document.getElementById('showErrors').onclick = showTestDetails;
+  document.getElementById('showSuccesses').onclick = showTestDetails;
 
   testRunner();
   showTestResults();
@@ -15,96 +15,143 @@ export function runTestsForHtml() {
 export function showTestResults() {
   const tResult = document.testResult;
   const attrs = ['testCount', 'testFailCount', 'runtimeErrorCount']
-  for (let i in attrs) {
-    const cntEl = document.getElementById(attrs[i]);
-    cntEl.textContent = tResult[attrs[i]];
+  for (let attr of attrs) {
+    const cntEl = document.getElementById(attr);
+    cntEl.textContent = tResult[attr];
   }
-  updateDetails();
+  showTestDetails();
 }
 
-/** Обновить детали тестов */
-function updateDetails() {
-  const detailEl = document.getElementById('testDetails');
-  detailEl.textContent = '';
+/** Показать детали тестов */
+function showTestDetails() {
+  const detailsEl = document.getElementById('testDetails');
+  detailsEl.textContent = '';
 
-  const showErrorsChecked = document.getElementById('showErrors').checked;
-  const showSuccessChecked = document.getElementById('showSuccesses').checked;
+  const needShowSuccess = document.getElementById('showSuccesses').checked;
+  const needShowErrors = document.getElementById('showErrors').checked;
+  let showDetails = needShowSuccess ? 'Success' : '';
+  showDetails += needShowErrors ? 'Error' : '';
 
-  let showType;
-  if (showSuccessChecked && showErrorsChecked) {
-    showType = 'Success && Error';
-  } else if (showSuccessChecked) {
-    showType = 'Success';
-  } else if (showErrorsChecked){
-    showType = 'Error';
-  } else {
-    showType = '';
-  }
-  processTestCalls(document.testResult.testCalls, showType);
+  const testCalls = selectTests(document.testResult.testCalls, showDetails);
+  processTests(testCalls, detailsEl);
 }
 
-/** Обрабатывает все результаты теста соглано критерия */
-function processTestCalls(testCalls, showType) {
-  if (showType === '') return;
+/** Сделать выборку тестов для отображения */
+function selectTests(testResult, showDetails) {
+  const resultCalls = {};
+  if (showDetails === '') return resultCalls;
 
-  for (let key in testCalls) {
-    const state = testCalls[key].state;
+  for (let key in testResult) {
+    const state = testResult[key].state;
     if (state !== undefined) {
-      if (showType.includes(state))
-        showDetails(testCalls[key], key);
+      if (showDetails.includes(state)) {
+        resultCalls[key] = testResult[key];
+      }
     } else {
-      if (key === '0') throw Error('arr');
-      processTestCalls(testCalls[key], showType);
+      const childResult = selectTests(testResult[key], showDetails);
+      if (Object.keys(childResult).length !== 0)
+        resultCalls[key] = childResult;
+    }
+  }
+  return resultCalls;
+}
+
+/** Обрабатывает все результаты теста */
+function processTests(testCalls, parentEl) {
+  for (let key in testCalls) {
+    const padding = parentEl.style.paddingLeft === '' ? '0px' : '40px';
+
+    const isTest = testCalls[key].state !== undefined;
+    if (isTest) {
+      const testResult = testCalls[key];
+      const scopeEl = createTestScope(key, testResult, padding);
+      parentEl.appendChild(scopeEl);
+      if (testResult.state !== 'Success') showErrorDetails(testResult, scopeEl);
+    } else {
+      const scopeEl = createDescribeScope(key, padding);
+      parentEl.appendChild(scopeEl);
+      processTests(testCalls[key], scopeEl);
     }
   }
 }
 
-/** Показывает результат выполнения теста в html странице. */
-function showDetails(testResult, fName) {
-  const detailEl = document.getElementById('testDetails');
-  if (testResult.state !== 'Success') {
-    appendError(testResult, fName);
-  } else {
-    detailEl.appendChild(createTestElement(`Success: ${fName} - success runned`, 'h1', 'test-detail'));
-  }
-  return detailEl.outerHTML;
+/** создать и вернуть элемент describe */
+function createDescribeScope(describeDescription, padding) {
+  const scopeEl = createScopeEl(padding);
+
+  const hEl = document.createElement('h1');
+  hEl.textContent = describeDescription;
+  hEl.style.margin = '0px';
+  setElAttribute(hEl, 'test-scope');
+
+  scopeEl.appendChild(hEl);
+  return scopeEl;
 }
 
-/** Добавляет ошибку в html страницy. */
-function appendError(testResult, fName) {
-  const detailEl = document.getElementById('testDetails');
-  const { err, callStack, errDesc } = testResult;
+/** создать и вернуть элемент test */
+function createTestScope(testDescription, testResult, padding) {
+  let funcDetail, clsName;
+  if (testResult.state === 'Success') {
+    funcDetail = 'success';
+    clsName = 'test-success';
+  } else {
+    funcDetail = testResult.err === 'TestError: ' ? 'test fail' : 'runtime error';
+    clsName = 'test-fail';
+  }
+  const spanIndex = testDescription.length + 1;
+  const funcHeaderEl = createTestElement(`${testDescription}: ${funcDetail}`, 'h1', clsName, spanIndex);
+  funcHeaderEl.style.margin = '0px';
 
-  const errType = err === 'TestError: ' ? 'test fail' : 'error'
-  const errDetail = `${err}${fName} - ${errType}`
-  const errEl = createTestElement(errDetail, 'h1', 'test-detail');
-  detailEl.appendChild(errEl);
-  errEl.appendChild(createTestElement(errDesc, 'h2', 'test-err'));
+  const scopeEl = createScopeEl(padding);
+  scopeEl.appendChild(funcHeaderEl);
+  return scopeEl;
+}
 
-  const stackBlockEl = document.createElement('div');
-  setElAttribute(stackBlockEl, 'test-stack');
-  errEl.appendChild(stackBlockEl);
+/** Создает и возвращает елемент scope */
+function createScopeEl(padding) {
+  const scopeEl = document.createElement('div');
+  if (padding !== '0px')
+    setElAttribute(scopeEl, 'test-scope');
+  scopeEl.style.paddingLeft = padding;
+  return scopeEl;
+}
+
+/** Добавляет в переданный scope детали ошибки */
+function showErrorDetails(testResult, scopeEl) {
+  const { callStack, errDesc } = testResult;
+
+  const spanIndex = errDesc.indexOf(': ') + 1;
+  scopeEl.appendChild(createTestElement(errDesc, 'h2', 'test-err', spanIndex));
+
+  const callStackEl = document.createElement('div');
+  setElAttribute(callStackEl, 'test-stack');
+  scopeEl.appendChild(callStackEl);
 
   callStack.split('\n').forEach((strItem) => {
     const txtLine = strItem.trim();
-    const prefixLen = txtLine.indexOf('(');
-    stackBlockEl.appendChild(createTestElement(txtLine, 'p', 'test-stack', prefixLen));
+    let spanIndex = txtLine.indexOf('(');
+    if (spanIndex === -1) spanIndex = txtLine.length;
+    callStackEl.appendChild(createTestElement(txtLine, 'p', 'test-stack', spanIndex));
   });
 }
 
-/** Создает на страничке html новый элемент теста. */
-function createTestElement(textContent, elType, clsName, prefixLength) {
-  const prefixLen = prefixLength ?? textContent.indexOf(': ') + 2;
-  const prefixText = textContent.substring(0, prefixLen);
-  const span = document.createElement('span');
-  span.textContent = textContent.substring(prefixLen);
-  setElAttribute(span, clsName);
+/** Создает и возвращает новый элемент деталей теста
+ * tagName - создаваемы элемент
+ * clsName (вместе с тегом элемента) влияет на стили отображения.
+ * spanIndex позволяет текс начиная с этого индекса обернуть в элемент span. */
+function createTestElement(textContent, tagName, clsName, spanIndex) {
+  const elText = textContent.substring(0, spanIndex);
+  const testEl = document.createElement(tagName);
+  setElAttribute(testEl, clsName);
+  testEl.textContent = elText;
 
-  const el = document.createElement(elType);
-  setElAttribute(el, clsName);
-  el.textContent = prefixText;
-  el.appendChild(span);
-  return el;
+  if (spanIndex < textContent.length) {
+    const span = document.createElement('span');
+    span.textContent = textContent.substring(spanIndex);
+    setElAttribute(span, clsName);
+    testEl.appendChild(span);
+  }
+  return testEl;
 }
 
 /** Установить атрибут html элемента. */
@@ -122,16 +169,16 @@ function getClsName(elType, clsName) {
   const token = `${elType}:${clsName}`;
   if (token === 'div:test') return 'w3-card'
   else if (token === 'div:test-stack') return 'w3-container, w3-margin-left, w3-light-gray';
-  else if (token === 'h1:test-detail') return 'w3-gray';
+  else if (token === 'div:test-scope') return 'w3-gray';
+  else if (token === 'h1:test-scope') return 'w3-gray';
+  else if (token === 'h1:test-success') return '';
+  else if (token === 'h1:test-fail') return '';
   else if (token === 'h2:test-err') return 'w3-khaki';
   else if (token === 'p:test-stack') return '';
-  else if (token === 'span:test-detail') return 'w3-text-green';
+  else if (token === 'span:test-scope') return 'w3-text-green';
+  else if (token === 'span:test-success') return 'w3-text-green';
+  else if (token === 'span:test-fail') return 'w3-text-red';
   else if (token === 'span:test-err') return 'w3-text-red';
   else if (token === 'span:test-stack') return 'w3-text-red';
   else throw Error('not valid element type and class name');
-}
-
-/** Обновить содержимое html элемента. */
-function updateElement(elem, elContent) {
-  elem.innerHTML = elContent;
 }
