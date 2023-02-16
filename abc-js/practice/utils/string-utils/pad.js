689e0a77498405e2c1f77ed335b9503feede9cbd
NurGeo <anzpro@gmail.com>
2023-02-16 11:46:08 +0500

    обновлен файл pad.js

--- abc-js/practice/utils/string-utils/pad.js
+++ abc-js/practice/utils/string-utils/pad.js
@@ -9,3 +9,9 @@ export function padStart(text, maxLength, fillString = ' ') {
  * Допускается в fillString передавать строку из нескольких символов. */
 export function padEnd(text, maxLength, fillString = ' ') {
 }
+
+/** Возвращает копию text увеличенный до длины maxLength
+ * заполненный в начале и конце символами fillString.
+ * Допускается в fillString передавать строку из нескольких символов. */
+export function pad(text, maxLength, fillString = ' ') {
+}
