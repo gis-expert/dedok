196cc06f91f553eed9be16b2221bd1d4b1774645
FredSteady23 <beisenov_12@mail.ru>
2023-02-17 14:43:25 +0500

    test

--- abc-js/practice/utils/number-utils/parse-integer.js
+++ abc-js/practice/utils/number-utils/parse-integer.js
@@ -1,2 +1,6 @@
 /** Возвращает целое числовое значение переданного аргумента value. */
-export function parseInteger(value) {}
+export function parseInteger(value) {
+    if (value === true) return 1;
+    if (value === false) return 0;
+    return parseInt(value);
+}
