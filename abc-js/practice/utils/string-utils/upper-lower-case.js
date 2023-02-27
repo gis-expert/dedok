fcf0fe27ed5e841fce1c9c6efec50a27dc75541e
NurGeo <anzpro@gmail.com>
2023-02-27 13:46:35 +0500

    Добавлены функции и тесты upperCase(), lowerCase()

---
+++ abc-js/practice/utils/string-utils/upper-lower-case.js
@@ -0,0 +1,7 @@
+/** возвращает копию строки text в верхнем регистре */
+export function upperCase(text) {
+}
+
+/** возвращает копию строки text в верхнем регистре */
+export function lowerCase(text) {
+}
