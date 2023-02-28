f5acf69ed675aebafea5e57a3a95f655a05d10f6
NurGeo <anzpro@gmail.com>
2023-02-28 13:48:03 +0500

    небольшие изменения в char-utils.js

--- abc-js/practice/utils/string-utils/common.js
+++ abc-js/practice/utils/string-utils/common.js
@@ -1,3 +1,23 @@
+/** для получения цифры строки по его индексу */
+export const DIGITS = '0123456789';
+export const SIGNS = '+-';
+export const FLOAT_DELIMITER = '.';
+
+export const ENG_UPPER_START = 65;
+export const ENG_UPPER_FINISH = 90;
+export const ENG_LOWER_START = 97;
+export const ENG_LOWER_FINISH = 122;
+export const ENG_CHAR_SHIFT = ENG_LOWER_START - ENG_UPPER_START;
+
+export const RUS_UPPER_START = 1040;
+export const RUS_UPPER_FINISH = 1071;
+export const RUS_LOWER_START = 1072;
+export const RUS_LOWER_FINISH = 1103;
+export const RUS_CHAR_SHIFT = RUS_LOWER_START - RUS_UPPER_START;
+
+export const DIGIT_START = 48;
+export const DIGIT_FINISH = 57;
+
 // --------------- for tests ------------------
 
 export const complexText = "Hello world!!! It's terminator";
