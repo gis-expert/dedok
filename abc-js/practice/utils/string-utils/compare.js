f41cc9fb9a1f7328864c1b66e949dbea6dbba8d3
KaisarSati <kaisar.aron20016@gmail.com>
2023-04-03 14:17:26 +0500

    Реализована и протестирована функция isEqual

--- abc-js/practice/utils/string-utils/compare.js
+++ abc-js/practice/utils/string-utils/compare.js
@@ -1,24 +1,53 @@
+import { notStringRiseError } from "./common.js";
+import { len } from "./len.js";
 
-/** возвращает булевый ответ равны ли параметры firstText и secondText. */
+
+
 export function isEqual(firstText, secondText) {
+    let i = len(firstText);
+    let b = len(secondText);
+      if (i !== b) return false;
+    const cycleLength = i  < b ? i  : b;
+    for (let index = 0; index < cycleLength; index += 1) {
+        if (firstText[index] !== secondText[index]) return false;
+    }   
+    return true;
 }
+
 
 /** возвращает true если аргументы не равны, и true в иных случаях. */
 export function isNotEqual(firstText, secondText) {
+    return !(isEqual(firstText, secondText));
 }
 
 /** возвращает булевый ответ больше ли параметр firstText чем secondText. */
 export function isMore(firstText, secondText) {
+    const cycleLength = len(firstText)  < len(secondText) ? len(firstText)  : len(secondText);
+    
+    for (let index = 0; index <= cycleLength; index += 1) {
+        if (firstText[index] > secondText[index]) { 
+            return true;
+        }
+        if (firstText[index] < secondText[index]) {
+            return false;
+    }
 }
+return len(firstText) > len(secondText);
+}
+
+
 
 /** возвращает булевый ответ меньше ли параметр firstText чем secondText. */
 export function isLess(firstText, secondText) {
+    return !(isEqual(firstText, secondText) || isMore(firstText, secondText));
 }
 
 /** возвращает булевый ответ больше или равно ли параметр firstText чем secondText. */
 export function isMoreOrEqual(firstText, secondText) {
+    return !(isLess(firstText, secondText));
 }
 
 /** возвращает булевый ответ меньше или равно ли параметр firstText чем secondText. */
 export function isLessOrEqual(firstText, secondText) {
+    return !(isMore(firstText, secondText));
 }
--- abc-js/practice/utils/string-utils/len.js
+++ abc-js/practice/utils/string-utils/len.js
@@ -1,2 +1,7 @@
 export function len(text) {
+    if (typeof text !== 'string' ) throw Error ('argument must be type of string');
+    let i;
+    for (i = 0; text[i]; i += 1) {
+    }
+    return i;
 }
