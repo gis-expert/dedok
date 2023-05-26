481d75b224a795acd6a99917663ea58988b55bb8
ZhanshinKurs~ <amirlan120304@gmail.com~>
2023-05-26 10:57:33 +0500

    исправление ошибок

--- abc-js/practice/utils/string-utils/compare.js
+++ abc-js/practice/utils/string-utils/compare.js
@@ -1,13 +1,17 @@
 import { len } from './len.js';
+import { isNotString } from './common.js';
 /** возвращает булевый ответ равны ли параметры firstText и secondText. */
 export function isEqual(firstText, secondText) {
 
-    if (typeof secondText !== typeof firstText) throw Error('argument must be type of string')
-    if (len(firstText) !== len(secondText)) return false
-    
+    const firstTextLength = len(firstText);
+    const secondTextLength = len(secondText);
 
-    const symlength = len(firstText) < len(secondText) ? len(firstText) : len(secondText);
-    for (let i = 0; i < symlength; i++) {
+    isNotString(firstText);
+
+    if (len(firstText) !== len(secondText)) return false
+    const minlength = firstTextLength < secondTextLength ? firstTextLength : secondTextLength;
+
+    for (let i = 0; i < minlength; i++) {
         if (firstText[i] !== secondText[i]) return false
     }
     return true
@@ -15,20 +19,19 @@ export function isEqual(firstText, secondText) {
 
 /** возвращает true если аргументы не равны, и true в иных случаях. */
 export function isNotEqual(firstText, secondText) {
-    if (typeof secondText !== typeof firstText) throw Error('argument must be type of string')
-    if (len(firstText) !== len(secondText)) return true
-    const symlength = len(firstText) < len(secondText) ? len(firstText) : len(secondText);
-    for (let i = 0; i < symlength; i++) {
-        if (firstText[i] !== secondText[i]) return true
-    }
-    return false
+    return !isEqual(firstText, secondText)
 }
 
 /** возвращает булевый ответ больше ли параметр firstText чем secondText. */
 export function isMore(firstText, secondText) {
-    if (typeof secondText !== typeof firstText) throw Error('argument must be type of string')
-    const symlength = len(firstText) > len(secondText) ? len(firstText) : len(secondText);
-    for (let i = 0; i < symlength; i++) {
+
+    const firstTextLength = len(firstText);
+    const secondTextLength = len(secondText);
+
+    isNotString(firstText);
+
+    const minlength = firstTextLength > secondTextLength ? firstTextLength : secondTextLength;
+    for (let i = 0; i < minlength; i++) {
         if ((firstText[i] > secondText[i] || secondText[i] === undefined)) return true
         if ((firstText[i] < secondText[i])) return false
     }
@@ -37,9 +40,14 @@ export function isMore(firstText, secondText) {
 
 /** возвращает булевый ответ меньше ли параметр firstText чем secondText. */
 export function isLess(firstText, secondText) {
-    if (typeof secondText !== typeof firstText) throw Error('argument must be type of string')
-    const symlength = len(firstText) > len(secondText) ? len(firstText) : len(secondText)
-    for (let i = 0; i < symlength; i++) {
+
+    const firstTextLength = len(firstText);
+    const secondTextLength = len(secondText);
+
+    isNotString(firstText);
+    
+    const minlength = firstTextLength > secondTextLength ? firstTextLength : secondTextLength;
+    for (let i = 0; i < minlength; i++) {
         if ((firstText[i] < secondText[i] || firstText[i] === undefined)) return true
         if ((firstText[i] > secondText[i])) return false
     }
@@ -48,9 +56,14 @@ export function isLess(firstText, secondText) {
 
 /** возвращает булевый ответ больше или равно ли параметр firstText чем secondText. */
 export function isMoreOrEqual(firstText, secondText) {
-    if (typeof firstText !== typeof secondText) throw Error ('argument must be type of string')
-    const symlength = len(firstText) > len(secondText) ? len(firstText) : len(secondText);
-    for (let i = 0; i < symlength; i++) {
+
+    const firstTextLength = len(firstText);
+    const secondTextLength = len(secondText);
+
+    isNotString(firstText);
+
+    const minlength = firstTextLength > secondTextLength ? firstTextLength : secondTextLength;
+    for (let i = 0; i < minlength; i++) {
         if ((firstText[i] > secondText[i])) return true
         if ((firstText[i] < secondText[i] || firstText[i] === undefined)) return false
     }
@@ -59,9 +72,13 @@ export function isMoreOrEqual(firstText, secondText) {
 
 /** возвращает булевый ответ меньше или равно ли параметр firstText чем secondText. */
 export function isLessOrEqual(firstText, secondText) {
-    if (typeof firstText !== typeof secondText) throw Error ('argument must be type of string')
-    const symlength = len(firstText) > len(secondText) ? len(firstText) : len(secondText);
-    for (let i = 0; i < symlength; i++) {
+
+    const firstTextLength = len(firstText);
+    const secondTextLength = len(secondText);
+
+    isNotString(firstText);
+    const minlength = firstTextLength > secondTextLength ? firstTextLength : secondTextLength;
+    for (let i = 0; i < minlength; i++) {
         if ((firstText[i] < secondText[i])) return true
         if ((firstText[i] > secondText[i] || secondText[i] === undefined)) return false
     }
