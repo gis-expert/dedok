e9f19b3ae6a4e2ca8acbe51908da027054223595
ZhanshinKurs~ <amirlan120304@gmail.com~>
2023-05-26 10:59:47 +0500

    исправление ошибок

--- abc-js/practice/utils/string-utils/len.js
+++ abc-js/practice/utils/string-utils/len.js
@@ -1,11 +1,11 @@
+import { isNotString } from "./common.js";
+
 export function len(text) {
-    console.log('len Active')   
-    console.log(`text: ${text}`)
-    if (typeof text !== 'string') throw Error ('argument must be type of string')
-    if(text.length === undefined) throw Error ('argument must be type of string')
-    let textCount = 0
-    for(let i = 0; i < 10000; i++){
-        if(text[i] === undefined) return textCount
-        textCount += 1; 
-    }
+
+    isNotString(text)
+
+    let textCount = 0;
+    for (; text[textCount]; textCount += 1) {}
+    return textCount;
 }
+
