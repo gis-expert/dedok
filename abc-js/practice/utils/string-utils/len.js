a007ff8a8f1f248a5a0685a703f4d16001a2cfcd
fjlakee <bissenovadilet@gmail.com>
2023-07-21 14:17:00 +0500

    len_test completed

--- abc-js/practice/utils/string-utils/len.js
+++ abc-js/practice/utils/string-utils/len.js
@@ -1,2 +1,23 @@
 export function len(text) {
+
+    if(typeof text === "undefined"){
+        throw Error("argument must be type of string");
+    }
+    else if(typeof text !== 'string' && typeof text !== 'undefined'){
+        throw Error("argument must be type of string");
+    }
+
+    console.log(typeof text);
+    
+    if(text.length === 0){
+        return 0;
+    }
+    else if(text.length === 1){
+        return 1;
+    }
+    else if(text.length > 1){
+        return text.length;
+    }
+
+
 }
