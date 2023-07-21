3d8b66ee1d896c5b9acd4f6cca36420051a5b827
fjlakee <bissenovadilet@gmail.com>
2023-07-21 13:34:28 +0500

    compare test complete

--- abc-js/practice/utils/string-utils/compare.js
+++ abc-js/practice/utils/string-utils/compare.js
@@ -1,24 +1,96 @@
-
 /** возвращает булевый ответ равны ли параметры firstText и secondText. */
 export function isEqual(firstText, secondText) {
+    if(typeof firstText !== "string" || typeof secondText !== "string"){
+        throw Error('argument must be type of string');
+    }
+    if(firstText === secondText){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 /** возвращает true если аргументы не равны, и false в иных случаях. */
 export function isNotEqual(firstText, secondText) {
+    if(typeof firstText !== "string" || typeof secondText !== "string"){
+        throw Error('argument must be type of string');
+    }
+    if(firstText === secondText){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
 
 /** возвращает булевый ответ больше ли параметр firstText чем secondText. */
 export function isMore(firstText, secondText) {
+    if(typeof firstText !== "string" || typeof secondText !== "string"){
+        throw Error('argument must be type of string');
+    }
+
+    if(firstText > secondText){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 /** возвращает булевый ответ меньше ли параметр firstText чем secondText. */
 export function isLess(firstText, secondText) {
+    if(typeof firstText !== "string" || typeof secondText !== "string"){
+        throw Error('argument must be type of string');
+    }
+
+    if(firstText > secondText){
+        return false;
+    }
+    else if(firstText === secondText){
+        return false;
+    }
+    else{
+        return true;
+    }
+
 }
 
 /** возвращает булевый ответ больше или равно ли параметр firstText чем secondText. */
 export function isMoreOrEqual(firstText, secondText) {
+    if(typeof firstText !== "string" || typeof secondText !== "string"){
+        throw Error('argument must be type of string');
+    }
+
+    if(firstText > secondText){
+        return true;
+    }
+    else if(firstText === secondText){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 /** возвращает булевый ответ меньше или равно ли параметр firstText чем secondText. */
 export function isLessOrEqual(firstText, secondText) {
+    if(typeof firstText !== "string" || typeof secondText !== "string"){
+        throw Error('argument must be type of string');
+    }
+
+    if(firstText > secondText){
+        return false;
+    }
+    else if(firstText === secondText){
+        return true;
+    }
+    else{
+        return true;
+    }
 }
